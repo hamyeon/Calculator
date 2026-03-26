de8a902baa6c94d6d18a22721510f067342c2c56
yeju321qaz <163377680+yeju321qaz@users.noreply.github.com>
2026-03-26 08:37:25 +0100

    Update add.py

--- api/add.py
+++ api/add.py
@@ -2,24 +2,14 @@ from http.server import BaseHTTPRequestHandler
 import json
 
 class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        response = json.dumps({
-            "message": "Backend is running. Send a POST request with num1 and num2."
-        }).encode("utf-8")
-
-        self.send_response(200)
-        self.send_header("Content-Type", "application/json")
-        self.send_header("Access-Control-Allow-Origin", "*")
-        self.end_headers()
-        self.wfile.write(response)
-
     def do_POST(self):
-        content_length = int(self.headers.get("Content-Length", 0))
+        content_length = int(self.headers['Content-Length'])
         body = self.rfile.read(content_length)
         data = json.loads(body)
 
         num1 = data.get("num1", 0)
         num2 = data.get("num2", 0)
+
         result = num1 + num2
 
         response = json.dumps({"result": result}).encode("utf-8")
@@ -27,8 +17,6 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-Type", "application/json")
         self.send_header("Access-Control-Allow-Origin", "*")
-        self.send_header("Access-Control-Allow-Headers", "Content-Type")
-        self.send_header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
         self.end_headers()
         self.wfile.write(response)
 
@@ -36,5 +24,5 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Access-Control-Allow-Origin", "*")
         self.send_header("Access-Control-Allow-Headers", "Content-Type")
-        self.send_header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+        self.send_header("Access-Control-Allow-Methods", "POST, OPTIONS")
         self.end_headers()
